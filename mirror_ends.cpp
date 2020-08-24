2f66d727fb2a2781c182bf114cf95e1d642661eb
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-24 18:16:24 +0900

    mirror_ends

---
+++ mirror_ends.cpp
@@ -0,0 +1,14 @@
+Run this code
+#include <iostream>
+#include <string>
+#include <algorithm>
+ 
+std::string mirror_ends(const std::string& in)
+{
+    return std::string(in.begin(),
+                       std::mismatch(in.begin(), in.end(), in.rbegin()).first);
+}
+
+int main()
+{
+}
